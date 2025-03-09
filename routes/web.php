70ffdc6a66adacc7d072f91d141dc9f388f930b1
sajadanari <sajaddehghananari@gmail.com>
2025-03-09 16:34:11 +0330

    Add Dashboard component and update layout structure

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Livewire\Dashboard\Dashboard;
 use App\Livewire\Home\Home;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', Home::class)->name('home');
+
+Route::get('/dashboard', Dashboard::class)->name('dashboard');
