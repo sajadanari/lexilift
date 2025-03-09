cf25d2d67b986812580efa3859308706546e293a
sajadanari <sajaddehghananari@gmail.com>
2025-03-09 17:09:17 +0330

    Add Google Fonts link for Material Symbols in Dashboard Layout

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -6,6 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <title>{{ $title ?? 'Page Title' }}</title>
+
+    <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght@400" rel="stylesheet" />
+
 </head>
 
 <body>
