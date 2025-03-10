6e4af17688c9f33350404524e0047384c43b811b
sajadanari <sajaddehghananari@gmail.com>
2025-03-10 16:58:35 +0330

    Create route for mywords component

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,11 @@
 
 use App\Livewire\Dashboard\Dashboard;
 use App\Livewire\Home\Home;
+use App\Livewire\MyWords\MyWords;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', Home::class)->name('home');
 
 Route::get('/dashboard', Dashboard::class)->name('dashboard');
+
+Route::get('/mywords', MyWords::class)->name('mywords');
