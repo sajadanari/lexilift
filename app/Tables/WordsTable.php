b536bbace16696d6ca4af11b9fc41569034f8021
sajadanari <sajaddehghananari@gmail.com>
2025-03-15 15:19:25 +0330

    Update WordsTable to format Part of Speech and Difficulty columns with labels

--- app/Tables/WordsTable.php
+++ app/Tables/WordsTable.php
@@ -13,7 +13,7 @@ class WordsTable extends DataTableComponent
     public function configure(): void
     {
         $this->setPrimaryKey('id')
-        
+
             ->setTableWrapperAttributes([
                 'default' => false,
                 'default-colors' => false,
@@ -88,10 +88,15 @@ class WordsTable extends DataTableComponent
                 ->sortable()
                 ->searchable(),
             Column::make('Part of Speech', 'part_of_speech')
-                ->sortable(),
+                ->sortable()
+                ->format(function($value) {
+                    return $value->label();
+                }),
             Column::make('Difficulty', 'difficulty_level')
-                ->sortable(),
-            
+                ->sortable()
+                ->format(function($value) {
+                    return $value->label();
+                }),
             Column::make('Actions', 'id')
                 ->format(function($value, $row, Column $column) {
                     return view('livewire.tables.words-table-actions', ['word' => $row]);
