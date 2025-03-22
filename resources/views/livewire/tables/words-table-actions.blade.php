b0e78d9470feda9007f84c40342f01525a861a88
sajadanari <141274694+sajadanari@users.noreply.github.com>
2025-03-22 11:23:37 -0700

    Remove WordsTable component and associated action views

--- resources/views/livewire/tables/words-table-actions.blade.php
+++
@@ -1,7 +0,0 @@
-<button wire:click="$dispatch('edit-word', { wordId: {{ $word->id }} })" class="inline-flex items-center hover:bg-gray-300 p-2 rounded cursor-pointer">
-    <i class="material-symbols-outlined">edit_square</i>
-</button>
-
-<button wire:click="$dispatch('delete-word', { wordId: {{ $word->id }} })" class="inline-flex items-center text-red-500 hover:bg-gray-300 p-2 rounded cursor-pointer">
-    <i class="material-symbols-outlined">delete</i>
-</button>
