6949e91954ad245c5d06d3fff913bfdb965978fd
sajadanari <141274694+sajadanari@users.noreply.github.com>
2025-03-22 00:25:16 -0700

    Enhance user actions UI with icon buttons for edit and delete; improve accessibility and visual consistency

--- resources/views/livewire/users/user-actions.blade.php
+++ resources/views/livewire/users/user-actions.blade.php
@@ -1,10 +1,14 @@
 <div class="flex space-x-2">
     @if($can_edit)
-        <a href="" class="text-blue-600 hover:text-blue-900">
-            Edit
+        <a href="" class="inline-flex text-white p-2 rounded-full hover:text-gray-900 hover:bg-gray-200">
+            <span class="material-symbols-outlined">
+                edit
+            </span>
         </a>
     @endif
-    <button wire:click="deleteUser({{ $item->id }})" class="text-red-600 hover:text-red-900">
-        Delete {{ $item->name }}
+    <button wire:click="deleteUser({{ $item->id }})" class="inline-flex text-red p-2 rounded-full hover:text-red-900 hover:bg-gray-200 cursor-pointer">
+        <span class="material-symbols-outlined">
+            delete
+        </span>
     </button>
 </div>
