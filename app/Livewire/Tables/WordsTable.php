d66834c4e905eb4c6c2aa591d8e099d65d4fc6da
sajadanari <141274694+sajadanari@users.noreply.github.com>
2025-03-22 06:26:57 -0700

    Refactor WordsTable component to extend BaseTable; implement searchable fields and customizable columns for improved functionality

--- app/Livewire/Tables/WordsTable.php
+++ app/Livewire/Tables/WordsTable.php
@@ -2,12 +2,40 @@
 
 namespace App\Livewire\Tables;
 
+use App\Models\Word;
+use Illuminate\Database\Eloquent\Builder;
 use Livewire\Component;
 
-class WordsTable extends Component
+class WordsTable extends BaseTable
 {
-    public function render()
+
+    protected function baseQuery(): Builder
     {
-        return view('livewire.tables.words-table');
+        return Word::query();
     }
+
+    protected function searchableFields(): array
+    {
+        return ['word', 'email'];
+    }
+
+    protected function columns(): array
+    {
+        return [
+            'word' => ['label' => 'Word', 'sortable' => true],
+            'part_of_speech' => ['label' => 'Part Of Speech', 'sortable' => true],
+            'dificulty' => ['label' => 'Dificulty', 'sortable' => true],
+            'actions' => [
+                'label' => 'Actions',
+                'view' => 'livewire.my-words.my-words-actions',
+                'params' => [
+                    'can_edit' => true
+                ],
+                'sortable' => false
+            ],
+            'created_at' => ['label' => 'Created At', 'sortable' => true],
+        ];
+    }
+
+
 }
