bd74e2f6ea1390371e2427d5a3d6f705f7cca226
sajadanari <141274694+sajadanari@users.noreply.github.com>
2025-03-22 00:28:01 -0700

    Replace edit link with button for user actions; enhance interactivity and maintainability

--- resources/views/livewire/users/user-actions.blade.php
+++ resources/views/livewire/users/user-actions.blade.php
@@ -1,10 +1,10 @@
 <div class="flex space-x-2">
     @if($can_edit)
-        <a href="" class="inline-flex text-white p-2 rounded-full hover:text-gray-900 hover:bg-gray-200">
+        <button wire:click="$parent.editUser({{ $item->id }})" class="inline-flex text-white p-2 rounded-full hover:text-gray-900 hover:bg-gray-200 cursor-pointer">
             <span class="material-symbols-outlined">
                 edit
             </span>
-        </a>
+        </button>
     @endif
     <button wire:click="deleteUser({{ $item->id }})" class="inline-flex text-red p-2 rounded-full hover:text-red-900 hover:bg-gray-200 cursor-pointer">
         <span class="material-symbols-outlined">
