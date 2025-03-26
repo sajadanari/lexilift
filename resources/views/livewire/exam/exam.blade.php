40d99e6a38c59560f44a1d521e93600126ad96bd
sajadanari <141274694+sajadanari@users.noreply.github.com>
2025-03-25 17:39:25 -0700

    Refactor exam UI for improved layout and styling, enhancing responsiveness and user interaction

--- resources/views/livewire/exam/exam.blade.php
+++ resources/views/livewire/exam/exam.blade.php
@@ -1,5 +1,5 @@
-<div class="min-h-screen bg-gray-50">
-    <div class="mx-auto px-4 py-8 max-w-4xl">
+<div class="min-h-screen">
+    <div class="mx-auto p-0 md:px-4 md:py-8 max-w-4xl">
         @if(!$isStarted)
             <div class="bg-white rounded-2xl shadow-lg p-8 text-center">
                 <div class="flex flex-col items-center justify-center space-y-8">
@@ -7,7 +7,7 @@
                         <span class="material-symbols-outlined text-[var(--accent-clr)] text-7xl animate-bounce">
                             school
                         </span>
-                        <span class="absolute -right-2 -top-2 bg-[var(--accent-clr)] text-white rounded-full p-2">
+                        <span class="absolute -right-2 -top-2 bg-[var(--accent-clr)] text-white rounded-full p-2 flex">
                             <span class="material-symbols-outlined text-xl">flag</span>
                         </span>
                     </div>
@@ -61,7 +61,7 @@
                         <button wire:click="nextQuestion"
                                 @class([
                                     'px-6 py-3 rounded-xl transition-all duration-300 w-full md:w-auto float-right flex items-center justify-center space-x-2',
-                                    'bg-[var(--accent-clr)] hover:bg-[var(--accent-clr-dark)] text-white cursor-pointer transform hover:scale-105' => $currentQuestionAnswered,
+                                    'bg-[var(--accent-clr)] hover:bg-gray-900 text-white cursor-pointer transform hover:scale-105' => $currentQuestionAnswered,
                                     'bg-gray-200 text-gray-400 cursor-not-allowed' => !$currentQuestionAnswered
                                 ])
                                 {{ !$currentQuestionAnswered ? 'disabled' : '' }}>
