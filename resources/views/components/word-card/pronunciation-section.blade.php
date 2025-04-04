3b02789ecde93fab7284920bccb5ee1102fbd7f7
sajadanari <141274694+sajadanari@users.noreply.github.com>
2025-04-04 06:11:25 -0700

    Turn review detail headings into h6 from h3

--- resources/views/components/word-card/pronunciation-section.blade.php
+++ resources/views/components/word-card/pronunciation-section.blade.php
@@ -2,7 +2,7 @@
 
 @if($word->pronunciation)
 <div class="mb-3 flex items-center gap-2">
-    <h3 class="text-sm font-medium text-gray-500">Pronunciation:</h3>
+    <h6 class="text-sm font-medium text-gray-500">Pronunciation:</h3>
     <span class="text-gray-700">/{{ $word->pronunciation }}/</span>
 
 </div>
--- resources/views/components/word-card/text-section.blade.php
+++ resources/views/components/word-card/text-section.blade.php
@@ -7,7 +7,7 @@
 ])
 
 <div class="mb-3">
-    <h3 class="text-sm font-medium text-gray-500 {{ $centerTitle ? 'text-center' : '' }}">{{ $title }}</h3>
+    <h6 class="{{ $centerTitle ? 'text-center' : '' }}">{{ $title }}</h3>
     <p class="text-gray-700 {{ $italic ? 'italic' : '' }} {{ $persian ? 'persian' : '' }}">
         {{ $italic ? '"'.$content.'"' : $content }}
     </p>
