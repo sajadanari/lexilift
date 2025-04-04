1829cc339be0002f16b6787d62e6ca5302b01478
sajadanari <141274694+sajadanari@users.noreply.github.com>
2025-04-04 08:13:54 -0700

    Refactor layout and structure of My Words page for improved responsiveness and organization

--- resources/views/livewire/my-words/index.blade.php
+++ resources/views/livewire/my-words/index.blade.php
@@ -1,20 +1,24 @@
 <div class="mx-auto px-4 py-8">
-    <div class="flex justify-between items-center mb-6">
-        <h1 class="text-2xl font-bold text-gray-800">My Words</h1>
+    <div class="container flex flex-col md:flex-row md:justify-between items-center space-y-6 md:space-y-0">
+        <h1 class="">My Words</h1>
         <x-forms.primary-btn
             wire:click="showCreatePage"
             icon="add"
+            class="w-full md:w-auto"
         >
             Add New Word
         </x-forms.primary-btn>
     </div>
 
-    @if (session()->has('message'))
-        <div class="bg-green-100 border-l-4 border-green-500 text-green-700 p-4 mb-4" role="alert">
-            {{ session('message') }}
-        </div>
-    @endif
+    <div class="container">
+        @if (session()->has('message'))
+            <div class="bg-green-100 border-l-4 border-green-500 text-green-700 p-4 mb-4" role="alert">
+                {{ session('message') }}
+            </div>
+        @endif
 
-    {{-- Words Table --}}
-    <livewire:tables.user-words-table />
+        {{-- Words Table --}}
+        <livewire:tables.user-words-table />
+    </div>
+
 </div>
